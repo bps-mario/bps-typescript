999a234430809fca436198e84a84d948f924e6d5
bps-tx4 <m.trevino@blueparrotsoftwarellc.com>
2024-03-25 13:48:39 -0500

    finished chapter two

---
+++ Chapter Two/src/main.ts
@@ -0,0 +1,18 @@
+let myName = 'Dave' // implicitly shown that it is string variable
+
+let myName2: string = "Dave" // explicitly shown that it is a string 
+
+let myName3: string ; //doesnt need to initalize value
+
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+const sum = (a: number, b: string) =>{
+    return a + b;
+}
