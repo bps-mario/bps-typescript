d446f5e62b06a53767964c20eb4ffcb280db9392
bps-tx4 <m.trevino@blueparrotsoftwarellc.com>
2024-03-25 14:55:34 -0500

    finished chapter 3

--- Chapter Three/build/js/main.js
+++ Chapter Three/build/js/main.js
@@ -1,14 +1,56 @@
 "use strict";
-let myName = 'Dave'; // implicitly shown that it is string variable
-let myName2 = "Dave"; // explicitly shown that it is a string 
-let myName3; //doesnt need to initalize value
-let meaningOfLife;
-let isLoading;
-let album;
-myName = 'John';
-meaningOfLife = 42;
-isLoading = true;
-album = 5150;
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'john';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push("Van Halen");
+bands.push;
+// Tuple specific length and specic data type for each element.
+let myTuple = ["Dave", 42, true];
+let mixed = ["John", 1, false];
+myTuple[2] = true;
+myTuple[1] = 1;
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {}; // my object is equal to a object ({})
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
 };
+exampleObj.prop1 = "John";
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", 'IV']
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`;
+    }
+};
+console.log(greetGuitarist(evh));
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level 
+//addition to JavaScript but something added to the language and runtime. "
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- Chapter Three/src/main.ts
+++ Chapter Three/src/main.ts
@@ -1,23 +1,85 @@
-let myName = 'Dave' // implicitly shown that it is string variable
+let stringArr = ['one', 'hey', 'Dave']
 
-let myName2: string = "Dave" // explicitly shown that it is a string 
+let guitars = ['Strat', 'Les Paul', 5150]
 
-let myName3: string ; //doesnt need to initalize value
+let mixedData = ['EVH', 1984, true]
 
-let meaningOfLife: number;
-let isLoading: boolean;
-let album;
+stringArr[0] = 'john'
+stringArr.push('hey')
 
-myName = 'John'
-meaningOfLife = 42
-isLoading = true
-album = 5150
+guitars[0] = 1984
+guitars.unshift('Jim')
 
-const sum = (a: number, b: string) =>{
-    return a + b;
+let test = []
+let bands: string[] = []
+bands.push("Van Halen")
+bands.push
+
+// Tuple specific length and specic data type for each element.
+let myTuple: [string, number, boolean] = ["Dave", 42, true]
+
+let mixed = ["John", 1, false]
+
+myTuple[2] = true 
+myTuple[1] = 1 
+
+
+// Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {} // my object is equal to a object ({})
+
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
 }
 
-let postId: string | number;
-let isActive: number | boolean | string; // not limited to only two data types
+exampleObj.prop1 = "John"
 
-let re: RegExp = /\w+/g
+//type annotation for objects
+
+// can also use interface instaed of type
+//when to use interface or when to use type? interface when it something like a class
+interface Guitarist {
+    name?: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+let evh:  Guitarist = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp:  Guitarist = {
+    name: "Jimmy",
+    active: true,
+    albums: ["I", "II", 'IV']
+}
+
+const greetGuitarist = (guitarist: Guitarist) =>{
+    if (guitarist.name){
+        return `Hello ${guitarist.name.toUpperCase()}!`
+    }
+}
+
+console.log(greetGuitarist(evh))
+
+
+// Enums
+// "Unlike most TypeScript features, Enums are not a type-level 
+//addition to JavaScript but something added to the language and runtime. "
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+
+}
+
+console.log(Grade.U);
