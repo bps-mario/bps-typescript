e225b21bd8a528126d7e8b165d1ff064f1771a45
bps-tx4 <m.trevino@blueparrotsoftwarellc.com>
2024-03-25 16:55:05 -0500

    stopped at chapter 5 almost done

---
+++ Chapter Five/build/js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+//convert
+let a = 'hello';
+let b = a; // assigned to a less specific type
+let c = a; // assigned to a more specific type
+// using angle brackets
+let d = 'world';
+let e = 'world';
+//angle brackets cant be used in jsx files which are in react
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(2, 2, 'concat');
+// Be Careful! TS sees no problem but a string is returned
+let myNextVal = addOrConcat(2, 2, 'concat');
+// althought typescript will still check asserstions when it can
+// 10 as string
+//forced casting or double casting
+10;
+//The DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
---
+++ Chapter Five/src/main.ts
@@ -0,0 +1,39 @@
+//type assertion or type casting
+type One = string
+type Two = string | number
+type Three = 'hello'
+
+//convert
+let a: One = 'hello'
+let b = a as Two // assigned to a less specific type
+let c = a as Three // assigned to a more specific type
+
+// using angle brackets
+let d = <One> 'world'
+let e = <string | number> 'world'
+//angle brackets cant be used in jsx files which are in react
+
+
+const addOrConcat = (a: number, b: number, c: 'add' | "concat"): number | string => {
+    if( c === 'add') return a + b
+    return '' + a + b
+
+}
+
+let myVal: string = addOrConcat(2,2,'concat') as string;
+
+// Be Careful! TS sees no problem but a string is returned
+let myNextVal: number = addOrConcat(2,2,'concat') as number;
+
+// althought typescript will still check asserstions when it can
+// 10 as string
+
+//forced casting or double casting
+(10 as unknown) as string
+
+//The DOM
+const img = document.querySelector('img') as HTMLImageElement
+const myImg = document.getElementById('#img')! as HTMLImageElement
+
+img.src
+myImg.src
