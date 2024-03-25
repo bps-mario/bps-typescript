06490781b69410fb15280369b32f29326b8b0915
bps-tx4 <m.trevino@blueparrotsoftwarellc.com>
2024-03-25 13:52:17 -0500

    saved chapter two and started on chapter 3

---
+++ Chapter Three/build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = 'Dave'; // implicitly shown that it is string variable
+let myName2 = "Dave"; // explicitly shown that it is a string 
+let myName3; //doesnt need to initalize value
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
